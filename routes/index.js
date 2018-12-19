32fdf2f49df4eaaf9405da6be4513a872b5fabe4
sudarshana.patil <patilsudarshan94@gmail.com>
2018-12-19 18:04:20 +0530

    create team API

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,50 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+const shortid = require('shortid');
+const async = require("async")
+
+//creates team and players keys
+router.post("/apis/createteam", (req, reply) => {
+  let { teamId,teamName, teamPlayers } = req.body;
+  
+  let counter = 1;
+  async.each(teamPlayers, (i, cb) => {
+    //set players data in redis
+    global.db.redis.set(`team::${teamId}::Player${counter++}`, "jjhj", (err, res) => {
+      if (err) {
+        console.log("Error in redis : ", err)
+        cb()
+      }
+      else {
+        console.log(`${teamId}::Player${counter++}`)
+        cb()
+      }
+    })
+  }, (done) => {
+    //set teamName
+    global.db.redis.set(`${teamId}::name`, teamName, (err, res) => {
+      if (err)
+        console.log("Error : ", err)
+      else {
+        console.log({ response: "created team successsfully" })
+        reply.send({ response: "created team successsfully" }).status(200);
+      }
+    })
+  })
 });
+
+//TODO
+// router.get("/apis/getPlayers", (req, reply) => {
+//   let teamId=req.params.teamId;
+//   global.db.redis.get(`${teamId}::name`, teamName, (err, res) => {
+//     if (err)
+//       console.log("Error : ", err)
+//     else {
+//       console.log({ response: "created team successsfully" })
+//       reply.send({ response: "created team successsfully" }).status(200);
+//     }
+//   })
+  
+// })
+
 module.exports = router;
