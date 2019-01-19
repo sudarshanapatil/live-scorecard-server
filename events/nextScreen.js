3fa7be68ea41c20504b5202772e8a5e447206701
Sudarshana Patil <patilsudarshana94@gmail.com>
2019-01-19 14:54:44 +0530

    added emit in nextScreen

--- events/nextScreen.js
+++ events/nextScreen.js
@@ -6,38 +6,38 @@ const nextScreen = (socket, redisClient) => {
         console.log("nextStstus", status)
         redisClient.setAsync('match::status', status)
         .catch((err) => console.log(err))
-       
-        if (status == 2) {
-            Promise.all([`team1::name`, `team2::name`])
-                .then(res => {
-                    global.userSocket.emit("nextScreen", {
-                        status: status,
-                        team1Name: res[0],
-                        team2Name: res[1]
-                    })
-                })
-        }
-        else if (status == 3 ^ status == 4) {
-            teamId = 1
-            if (status == 4)
-                teamId = 2
+        userSocket.emit(parseInt(status)+1)
+        // if (status == 2) {
+        //     Promise.all([`team1::name`, `team2::name`])
+        //         .then(res => {
+        //             global.userSocket.emit("nextScreen", {
+        //                 status: status,
+        //                 team1Name: res[0],
+        //                 team2Name: res[1]
+        //             })
+        //         })
+        // }
+        // else if (status == 3 ^ status == 4) {
+        //     teamId = 1
+        //     if (status == 4)
+        //         teamId = 2
 
-            let nameArr = []
-            redisClient.keysAsync(`team${teamId}::player*`)
-                .then(res => {
-                    res.map(i => {
-                        redisClient.hmgetAsync(i)
-                            .then(res => {
-                                nameArrp.push(res.name)
-                            })
-                    })
-                    global.userSocket.emit(`nextScreen`,{team:teamId,names:nameArr})
-                })
-        }
+        //     let nameArr = []
+        //     redisClient.keysAsync(`team${teamId}::player*`)
+        //         .then(res => {
+        //             res.map(i => {
+        //                 redisClient.hmgetAsync(i)
+        //                     .then(res => {
+        //                         nameArrp.push(res.name)
+        //                     })
+        //             })
+        //             global.userSocket.emit(`nextScreen`,{team:teamId,names:nameArr})
+        //         })
+        // }
 
-        else if (status == 6) { //send data to UserBoard
-            initializeEvent(global.userSocket, redisClient)
-        }
+        // else if (status == 6) { //send data to UserBoard
+        //     initializeEvent(global.userSocket, redisClient)
+        // }
        
     });
 }
