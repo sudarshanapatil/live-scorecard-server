3b97d030d8e91d601ed89b3326472533dd3d107a
Sudarshana Patil <patilsudarshana94@gmail.com>
2019-01-26 23:46:32 +0530

    added matchEnd event

--- events/inningEnd.js
+++ events/inningEnd.js
@@ -7,8 +7,21 @@ const inningEnd = (socket, redisClient) => {
         try { global.userSocket.emit(`inningEnd`, data) }
         catch (e) { console.log(e) }
 
-        let { teamId, totalScore, totalWicket } = data;
+        let { teamId, totalScore, totalWicket, inningId } = data;
         console.log('Status : ', status);
+        if (inningId == 2) {
+
+            Promise.all([redisClient.setAsync(`team1::score`, 0),
+            redisClient.setAsync(`team2::score`, 0),
+            redisClient.setAsync(`team1::wickets`, 0),
+            redisClient.setAsync(`team2::wickets`, 0),
+            redisClient.setAsync(`match::overs`, 0),
+            redisClient.mhsetAsync(`team1::info`, { teamName: "", teamLogo: "" }),
+            redisClient.mhsetAsync(`team1::info`, { teamName: "", teamLogo: "" })])
+                .then(res => console.log(res))
+                .catch(err => console.log(err))
+
+        }
         redisClient.setAsync(`team${teamId}::score`, totalScore)
             .catch((err) => { console.log("err: ", err) })
         redisClient.setAsync(`team${teamId}::wicket`, totalWicket)
